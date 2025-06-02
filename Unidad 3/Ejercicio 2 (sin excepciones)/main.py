601d04d98a31ec6ed9de16b3b0753fd1d2bd406d
Santiago Sanchez <167319346+Zantyhago@users.noreply.github.com>
2025-06-02 10:12:36 -0300

    Add files via upload

--- Unidad 3/Ejercicio 2 (sin excepciones)/main.py
+++ Unidad 3/Ejercicio 2 (sin excepciones)/main.py
@@ -1,1 +1,29 @@
-a
+from gestorBibliotecas import GestorBiblio
+
+def menu():
+    op = input('''Menu de opciones:
+               a) Agregar un libro.
+               b) Eliminar un libro.
+               c) Ubicar un libro.
+               d) Listar todos los libros.
+               z) Salir.
+               elejí pibe: ''')
+    while op != 'z':
+        if op == 'a':
+            xnombre = input("Ingrese nombre de la biblioteca: ")
+            gb.incisoA(xnombre)
+        elif op == 'b':
+            xnombre = input("Ingrese nombre de la biblioteca: ")
+            gb.incisoB(xnombre)
+        elif op == 'c':
+            xtitulo = input("Ingrese titulo de la obra: ")
+            gb.incisoC(xtitulo)
+        elif op == 'd':
+            gb.incisoD()
+        menu()
+    print("Gracias por coso")
+
+if __name__ == '__main__':
+    gb = GestorBiblio()
+    gb.leeDatos()
+    menu()
