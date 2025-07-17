a25e009256255d81402e4c175b322a24aadb26e6
Santiago Sanchez <167319346+Zantyhago@users.noreply.github.com>
2025-07-17 11:10:41 -0300

    Add files via upload

--- Unidad 3/Ejemplo de Lista Enlazada/classLista.py
+++ Unidad 3/Ejemplo de Lista Enlazada/classLista.py
@@ -25,19 +25,6 @@ class Lista:
             dato = self.__actual.getDato()
             self.__actual = self.__actual.getSiguiente()
             return dato
-        
-    def __iter__(self):
-        return self
-    def __next__(self):
-        if self.__indice == self.__tope:
-            self.__actual = self.__comienzo
-            self.__indice = 0
-            raise StopIteration
-        else:
-            self.__indice += 1
-            dato = self.__actual.getDato()
-            self.__actual = self.__actual.getSiguiente()
-            return dato
 
     def agregaProfesor(self, profesor):
         nodo = Nodo(profesor)
