f1bbe54987990b0e21fdd11588c41e9d87abeaf2
Santiago Sanchez <167319346+Zantyhago@users.noreply.github.com>
2025-07-19 18:50:44 -0300

    Update classPlan.py

--- Unidad 3/Ejercicio 4/classPlan.py
+++ Unidad 3/Ejercicio 4/classPlan.py
@@ -11,10 +11,6 @@ class Plan(abc.ABC):
         self.__cobertura = xcob
         self.__precio = xprec
 
-    @abc.abstractmethod
-    def getTipo(self):
-        pass
-
     def getCompañia(self):
         return self.__compania
     
@@ -32,5 +28,5 @@ class Plan(abc.ABC):
         pass
 
     def __str__(self):
-        return f"{self.getTipo():<15}{self.getCompañia():<19}{self.getDuracion():<11}{self.getCobertura():<27}{self.getPrecio():.2f}"
-        # en vez de hacer getTipo(), se podría un type(self) directamente. No avivo mas
+        return f"{self.__class__.__name__:<15}{self.getCompañia():<19}{self.getDuracion():<11}{self.getCobertura():<27}{self.getPrecio():.2f}"
+        # self.__class__.__name__ devuelve el nombre de la clase
