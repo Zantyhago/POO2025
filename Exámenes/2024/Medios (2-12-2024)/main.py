2eb72a98fb45928b41952fe47f2f69cde9706f8f
Santiago Sanchez <167319346+Zantyhago@users.noreply.github.com>
2025-07-26 12:01:10 -0300

    Add files via upload

--- Exámenes/2024/Medios (2-12-2024)/main.py
+++ Exámenes/2024/Medios (2-12-2024)/main.py
@@ -1,1 +1,41 @@
+from gestorMedios import GestorMedios
+from datetime import date, datetime
 
+def menu(op = 'y'):
+    try:
+        while op != 'z':
+            op = input('''                            MENÚ DE OPCIONES:
+                            A) Agregar medio manualmente.
+                            B) Mostrar informacion de un programa indicado:
+                            C) Mostrar información de todos los medios
+                            Z) Salir.
+                            Ingrese su opción -> ''').lower()
+            if op == 'a':
+                gm.incisoA()
+            elif op == 'b':
+                xnombP = input("Ingrese nombre del programa: ").lower()
+                gm.incisoB(xnombP)
+            elif op == 'c':
+                gm.incisoC()
+            else:
+                if op != 'z':
+                    print("Opción mal ingresada. Intente nuevamente.")
+    except IndexError as a:
+        print(a)
+        menu()
+    except ValueError as b:
+        print(b)
+        menu()
+    except TypeError as c:
+        print(c)
+        menu()
+    except Exception as d:
+        print(d)
+        menu()
+    else:
+        print("bueno eso es todo")    
+
+if __name__ == '__main__':
+    gm = GestorMedios()
+    gm.leeMedios()
+    menu()
